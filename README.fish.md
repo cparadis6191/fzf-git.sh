7e088998774ace7db593cf6abd7e5d7dd69e4c3d
Chad Paradis <cparadis6191@gmail.com>
2025-08-19 23:13:42 -0400

    Remove `bin/`
    Also, remove `bin/` from `default.nix` and `README.fish.md`.

--- README.fish.md
+++ README.fish.md
@@ -2,14 +2,6 @@
 
 ## Installation Instructions
 
-Run the following command to create symbolic links in the local bin directory
-to the tools contained in this repository:
-
-```
-$ stow --target="$HOME/.local/bin" \
-> bin
-```
-
 Run the following command to add fish key bindings to the tools contained in
 this repository:
 
--- default.nix
+++ default.nix
@@ -13,22 +13,7 @@ stdenv.mkDerivation {
 
     mkdir --parents $out/bin/
 
-    install -D ./bin/__fzf_git          $out/bin/
-    install -D ./bin/__fzf_git_cat      $out/bin/
-    install -D ./bin/__fzf_git_color    $out/bin/
-    install -D ./bin/__fzf_git_pager    $out/bin/
-    install -D ./bin/_fzf_git_branches  $out/bin/
-    install -D ./bin/_fzf_git_check     $out/bin/
-    install -D ./bin/_fzf_git_each_ref  $out/bin/
-    install -D ./bin/_fzf_git_files     $out/bin/
-    install -D ./bin/_fzf_git_fzf       $out/bin/
-    install -D ./bin/_fzf_git_hashes    $out/bin/
-    install -D ./bin/_fzf_git_lreflogs  $out/bin/
-    install -D ./bin/_fzf_git_remotes   $out/bin/
-    install -D ./bin/_fzf_git_stashes   $out/bin/
-    install -D ./bin/_fzf_git_tags      $out/bin/
-    install -D ./bin/_fzf_git_worktrees $out/bin/
-    install -D ./fzf-git.sh             $out/bin/
+    install -D ./fzf-git.sh $out/bin/
 
     mkdir --parents $out/share/fzf-git.sh/
 
