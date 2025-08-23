0688daf11b2bd025195e61802f215985da65089e
Chad Paradis <cparadis6191@gmail.com>
2025-08-23 14:30:55 -0400

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,23 @@
+{ fzf, git, stdenv, ... }:
+
+stdenv.mkDerivation {
+  pname = "fzf-git-sh";
+  version = "0.0.1";
+
+  src = ./.;
+
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir --parents $out/share/fzf-git.sh/
+
+    install -D ./fzf-git.fish $out/share/fzf-git.sh/fzf-git.fish
+    install -D ./fzf-git.sh   $out/share/fzf-git.sh/fzf-git.sh
+
+    runHook postInstall
+  '';
+
+  outputs = [ "out" ];
+}
